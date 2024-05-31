fd27ec506d01144a34fbdd8b8f0227d89f6ca42f
Roberto Alvarado <roberto.alvarado.35@gmail.com>
2024-05-31 15:01:28 -0600

    Tes notificacion

--- app/Console/Commands/NotificationAppointments.php
+++ app/Console/Commands/NotificationAppointments.php
@@ -31,21 +31,46 @@ class NotificationAppointments extends Command
     {
         //
         date_default_timezone_set("America/El_Salvador");
-        $simulet_hour_number = date("2024-05-31 14:01:35");//strtotime(date("2023-10-24 09:35:35"));
-        $appointments = Appointment::whereDate("date_appointment","2024-05-31")//now()->format("Y-m-d")
-                                    ->where("status",1)
-                                    //->where("cron_state",1)
-                                    ->get();
+        //$simulet_hour_number = date("2024-05-31 15:01:35");//strtotime(date("2023-10-24 09:35:35"));
+        $simulet_hour_number = date(now()->format("Y-m-d H:i:s"));
+
+
+
+        // $appointments = Appointment::whereDate("date_appointment","2024-05-31")//now()->format("Y-m-d")
+        //                             ->where("status",1)
+        //                             //->where("cron_state",1)
+        //                             ->get();
+
+        $now = now()->format("Y-m-d");
+
+        $appointments = Appointment::whereDate("date_appointment",$now)//now()->format("Y-m-d")
+        ->where("status",1)
+        //->where("cron_state",1)
+        ->get();
+
+        //dd($appointments);
+
+
         $now_time_number = strtotime($simulet_hour_number);//now()->format("Y-m-d h:i:s");//
+        $now_time_number_test = ($simulet_hour_number);//now()->format("Y-m-d h:i:s");//
+        //$now_time_number = strtotime($simulet_hour_number);//;//
+
+
         $patients = collect([]);
         foreach ($appointments as $key => $appointment) {
+
             $hour_start = $appointment->doctor_schedule_join_hour->doctor_schedule_hour->hour_start;
             $hour_end = $appointment->doctor_schedule_join_hour->doctor_schedule_hour->hour_end;
-
+            error_log($hour_start.' ---- '.$hour_end.' ---- '. $now_time_number_test);
             // 2023-10-25 08:30:00 -> 2023-10-25 07:30:00
-            $hour_start = strtotime(Carbon::parse("2024-05-31"." ".$hour_start)->subHour());
-            $hour_end = strtotime(Carbon::parse("2024-05-31"." ".$hour_end)->subHour());
-            // error_log($hour_start.' ---- '.$hour_end.' ---- '.$simulet_hour_number);
+            //$hour_start = strtotime(Carbon::parse("2024-05-31"." ".$hour_start)->subHour());
+            $hour_start = strtotime(Carbon::parse($now." ".$hour_start)->subHour());
+            $hour_end = strtotime(Carbon::parse($now." ".$hour_end)->subHour());
+
+            //error_log($hour_start.' ---- '.$hour_end.' ---- '.$simulet_hour_number);
+            error_log($hour_start.' ---- '.$hour_end.' ---- '.$now_time_number);
+
+
             if($hour_start <= $now_time_number && $hour_end >= $now_time_number){
                 $patients->push([
                     "name" => $appointment->patient->name,
