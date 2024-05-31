fe9227ae3f1c26623fdf7cb8c2a211252a9c9bf2
Roberto Alvarado <roberto.alvarado.35@gmail.com>
2024-05-31 13:15:54 -0600

    Se agrego todo el codigo que faltaba Notification Appointment

--- app/Console/Commands/NotificationAppointments.php
+++ app/Console/Commands/NotificationAppointments.php
@@ -27,19 +27,44 @@ class NotificationAppointments extends Command
     /**
      * Execute the console command.
      */
-    public function handle()    {        
-            date_default_timezone_set("America/El_Salvador");
-            $patient = [
-                "name"  => "Roberto",
-                "surname" => "Alvarado Lopez",
-                "avatar" => NULL,
-                "email" => "roberto.alvarado.35@gmail.com",
-                "mobile" => "71293626",
-                "specialitie_name" => "CONSULTORIA GENERAL",
-                "n_document" => "028084604",
-                "hour_start_format" => "2024-05-31 10:00:00",
-                "hour_end_format" =>  "2024-05-31 10:00:00",
-            ];
+    public function handle()
+    {
+        //
+        date_default_timezone_set("America/El_Salvador");
+        $simulet_hour_number = date("2024-05-31 13:01:35");//strtotime(date("2023-10-24 09:35:35"));
+        $appointments = Appointment::whereDate("date_appointment","2024-05-31")//now()->format("Y-m-d")
+                                    ->where("status",1)
+                                    //->where("cron_state",1)
+                                    ->get();
+        $now_time_number = strtotime($simulet_hour_number);//now()->format("Y-m-d h:i:s");//
+        $patients = collect([]);
+        foreach ($appointments as $key => $appointment) {
+            $hour_start = $appointment->doctor_schedule_join_hour->doctor_schedule_hour->hour_start;
+            $hour_end = $appointment->doctor_schedule_join_hour->doctor_schedule_hour->hour_end;
+            
+            // 2023-10-25 08:30:00 -> 2023-10-25 07:30:00
+            $hour_start = strtotime(Carbon::parse("2024-05-31"." ".$hour_start)->subHour());
+            $hour_end = strtotime(Carbon::parse("2024-05-31"." ".$hour_end)->subHour());
+            // error_log($hour_start.' ---- '.$hour_end.' ---- '.$simulet_hour_number);
+            if($hour_start <= $now_time_number && $hour_end >= $now_time_number){
+                $patients->push([
+                    "name" => $appointment->patient->name,
+                    "surname" => $appointment->patient->surname,
+                    "avatar" => $appointment->avatar ? env("APP_URL")."storage/".$appointment->avatar : NULL,
+                    "email" => $appointment->patient->email,
+                    "mobile" => $appointment->patient->mobile,
+                    "specialitie_name" => $appointment->specialitie->name,
+                    "n_document" => $appointment->patient->n_document,
+                    "hour_start_format" => Carbon::parse(date("Y-m-d")." ".$appointment->doctor_schedule_join_hour->doctor_schedule_hour->hour_start)->format("h:i A"),
+                    "hour_end_format" => Carbon::parse(date("Y-m-d")." ".$appointment->doctor_schedule_join_hour->doctor_schedule_hour->hour_end)->format("h:i A"),
+                ]);
+            }
+            //$appointment->update(["cron_state" => 2]);
+        }
+        foreach ($patients as $key => $patient) {
             Mail::to($patient["email"])->send(new NotificationAppoint($patient));
+        }
+        
+        // dd($patients);
     }
 }
